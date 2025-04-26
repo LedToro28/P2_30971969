b719dd14c3d02ce6c801dd3aeb1842a8da74e196
Heshmert <heshmert@protonmail.com>
2025-04-26 12:17:46 -0400

    mas-vistas

--- src/app.ts
+++ src/app.ts
@@ -4,24 +4,25 @@ import path from 'path';
 const app = express();
 const port = 3000;
 
-// Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, '../public')));
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
-// Definir los datos a mostrar
-const nombreCompleto = "Juan Pablo Gonzalez La Posta";
-const cedula = "V-31499269";
-const seccion = "3"; 
 
-// Implementar la ruta para la página inicial
-app.get('/', (req: Request, res: Response) => {
-    res.render('index', { nombreCompleto, cedula, seccion });
+app.get('/', (req, res) => {
+    res.render('index', {});
+  });
+app.get('/servicios', (req, res) => {
+    res.render('servis', {});
+  });
+app.get('/contacto', (req, res) => {
+    res.render('contac', {});
+  });   
+app.get('/info', (req, res) => {
+    res.render('info', {});
+  });
 
-});
 
-// Iniciar el servidor
 app.listen(port, () => {
     console.log(`Servidor corriendo en ${port}`);
 });
